88fc1b29137b523f2f8e991eecc2b3bdac9e828d
leoddiez <mxm220208@utdallas.edu>
2026-05-06 16:26:28 +0000

    wrote the node class and header class

--- main.java
+++ main.java
@@ -2,45 +2,80 @@ import java.io.*;
 import java.nio.*;
 import java.nio.file.*;
 
-public static void main(String[] args) {
-  if(args.length < 1) {
-    Sys.err.println("Unknown command");
-    System.exit(1);
+public class P3 {
+
+  static final int BLOCK_SIZE = 512;
+  static final int MIN_DEGREE = 10;
+  static final int MAX_KEYS = 19;
+  static final int MAX_CHILD = 20;
+
+  class Header {
+    long root;
+    long nextBlock;
+    byte[] MAGICIAN = "4348PRJ3";
+
+    static void getBytes() {
+      return;
+    }
+
   }
-  switch (args[0].toLowerCase()) { // I have no idea if this is allowed yet, or if they use capital letters i reject, but imma just convert for my sanity
-    case "create":
-      if(args.length < 2) {Sys.err.println("Usage: create <file>"); System.exit(1);}
-      fCreate(args[1]);
-      break;
 
-    case "insert":
-      if(args.length < 4) {Sys.err.println("Usage: insert <file> <key> <value>"); System.exit(1);}
-      fInsert(args[1], args[2], args[3]);
-      break;
+  static class Node {
+    long bID;
+    long pID;
+    long pairs;
+    long[] keys = new long[MAX_KEYS];
+    long[] val = new long[MAX_KEYS];
+    long[] child = new long[MAX_CHILD];
 
-    case "search":
-      if(args.length < 3) {Sys.err.println("Usage: search <file> <key>"); System.exit(1);}
-      fSearch(args[1], args[2]);
-      break;
+    Node(long bID) {
+      this.bID = bID;
+      this.pID = 0;
+      this.pairs = 0;
+    }
 
-    case "load":
-      if(args.length < 3) {Sys.err.println("Usage: load <file> <csvfile>"); System.exit(1);}
-      fSearch(args[1], args[2]);
-      break;
+  }
 
-    case "print":
-      if(args.length < 2) {Sys.err.println("Usage: print <file>"); System.exit(1);}
-      fPrint(args[1]);
-      break;
-
-    case "extract":
-      if(args.length < 3) {Sys.err.println("Usage: extract <file> <csvfile>"); System.exit(1);}
-      fExtract(args[1], args[2]);
-      break;
-
-    default:
-      Sys.err.println("Unknown command: " + args[0]);
+  public static void main(String[] args) {
+    if(args.length < 1) {
+      System.err.println("Unknown command");
       System.exit(1);
-  }
+    }
+    switch (args[0].toLowerCase()) { // I have no idea if this is allowed yet, or if they use capital letters i reject, but imma just convert for my sanity
+      case "create":
+        if(args.length < 2) {System.err.println("Usage: create <file>"); System.exit(1);}
+        fCreate(args[1]);
+        break;
 
+      case "insert":
+        if(args.length < 4) {System.err.println("Usage: insert <file> <key> <value>"); System.exit(1);}
+        fInsert(args[1], args[2], args[3]);
+        break;
+
+      case "search":
+        if(args.length < 3) {System.err.println("Usage: search <file> <key>"); System.exit(1);}
+        fSearch(args[1], args[2]);
+        break;
+
+      case "load":
+        if(args.length < 3) {System.err.println("Usage: load <file> <csvfile>"); System.exit(1);}
+        fLoad(args[1], args[2]);
+        break;
+
+      case "print":
+        if(args.length < 2) {System.err.println("Usage: print <file>"); System.exit(1);}
+        fPrint(args[1]);
+        break;
+
+      case "extract":
+        if(args.length < 3) {System.err.println("Usage: extract <file> <csvfile>"); System.exit(1);}
+        fExtract(args[1], args[2]);
+        break;
+
+      default:
+        System.err.println("Unknown command: " + args[0]);
+        System.exit(1);
+    }
+
+  }
 }
