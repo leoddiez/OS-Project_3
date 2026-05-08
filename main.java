2f167758835a0d47a17f0cafa2708fb01277548c
leoddiez <mxm220208@utdallas.edu>
2026-05-08 04:38:25 +0000

    main is hopefully fully complete, but unsure

--- main.java
+++ main.java
@@ -15,42 +15,48 @@ public class P3 {
       case "create":
         if(args.length != 2) {System.err.println("Usage: create <file>"); System.exit(1);}
         fname = args[1];
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
         try(RandomAccessFile f = new RandomAccessFile(fname, "rw")) {
           Header h = new Header();
           h.write(f);
           System.out.println("Created index file: " + fname);
-        } catch (IOException e) {System.err.println("Something in create went wrong");}
+        } catch (IOException e) {System.err.println("Erorr in create: " + e.getMessage()); System.exit(1);}
         break;
 
       case "insert":
         if(args.length != 4) {System.err.println("Usage: insert <file> <key> <value>"); System.exit(1);}
         fname = args[1];
-        key =  Long.parseLong(args[2]);
-        val = Long.parseLong(args[3]);
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
+        key =  Long.parseUnsignedLong(args[2]);
+        val = Long.parseUnsignedLong(args[3]);
         try(RandomAccessFile f = new RandomAccessFile(fname, "rw")) {
           Header h = Header.read(f);
           new BTree(f, h).insert(key, val);
-        } catch (IOException e) {System.err.println("Something in insert went wrong");}
+        } catch (IOException e) {System.err.println("Erorr in insert: " + e.getMessage()); System.exit(1);}
         break;
 
       case "search":
         if(args.length != 3) {System.err.println("Usage: search <file> <key>"); System.exit(1);}
         fname = args[1];
-        key = Long.parseLong(args[2]);
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
+        key = Long.parseUnsignedLong(args[2]);
         try(RandomAccessFile f = new RandomAccessFile(fname, "rw")) {
           Header h = Header.read(f);
           long[] results = new BTree(f, h).search(key);
-          if (results == null) {System.out.println("Key: " + key + " not found");} else {
-            System.out.println(Long.toUnsignedString(results[0] + "," + Long.toUnsignedString(results[1])));
+          if (results == null) {
+            System.out.println("Key: " + Long.toUnsignedString(key) + " not found/valid");
+          } else {
+            System.out.println(Long.toUnsignedString(results[0]) + "," + Long.toUnsignedString(results[1]));
           }
-        } catch (IOException e) {System.err.println("Something in search went wrong");}
+        } catch (IOException e) {System.err.println("Erorr in search: " + e.getMessage()); System.exit(1);}
         break;
 
       case "load":
         if(args.length != 3) {System.err.println("Usage: load <file> <csvfile>"); System.exit(1);}
-        if (!new File(csv).esists()) {System.err.println("Csv file: " + csv + " not found"); System.exit(1);}
         fname = args[1];
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
         csv = args[2];
+        if (!new File(csv).exists()) {System.err.println("Csv file: " + csv + " not found/valid"); System.exit(1);}
         try(RandomAccessFile f = new RandomAccessFile(fname, "rw");
           BufferedReader br = new BufferedReader(new FileReader(csv))) {
             Header h = Header.read(f);
@@ -61,19 +67,43 @@ public class P3 {
               cnt++;
               parse = parse.trim();
               if (parse.isEmpty()) {continue;}
-              
+              String[] parts = parse.split(",");
+              if(parts.length <2) {System.err.println("Warning: Skipping line " + cnt); continue;}
+              try {
+                tree.insert(Long.parseUnsignedLong(parts[0].trim()), Long.parseUnsignedLong(parts[1].trim()));
+              } catch (NumberFormatException e) {System.err.println("Warning: Line skipped with bad numbers " + cnt);}
             }
-          }
+            System.out.println(csv + " loaded");
+          } catch (IOException e) {System.err.println("Erorr in load: " + e.getMessage()); System.exit(1);}
         break;
 
       case "print":
         if(args.length != 2) {System.err.println("Usage: print <file>"); System.exit(1);}
-        BTree.fPrint(args[1]);
+        fname = args[1];
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
+        try(RandomAccessFile f = new RandomAccessFile(fname, "rw")) {
+          Header h = Header.read(f);
+          if(h.root == 0) {System.out.println("...aint nuthin in the index bro..."); break;}
+          BTree tree = new BTree(f, h);
+          tree.inorder((k, v) -> {
+            System.out.println(Long.parseUnsignedLong(k) + "," + Long.parseUnsignedLong(v));
+          });
+        } catch (IOException e) {System.err.println("Erorr in print: " + e.getMessage()); System.exit(1);}
         break;
 
       case "extract":
         if(args.length != 3) {System.err.println("Usage: extract <file> <csvfile>"); System.exit(1);}
-        BTree.fExtract(args[1], args[2]);
+        fname = args[1];
+        if (!new File(fname).exists()) {System.err.println("File: " + fname + " not found/valid"); System.exit(1);}
+        csv = args[2];
+        if (!new File(csv).exists()) {System.err.println("File: " + csv + " not found/valid"); System.exit(1);}        
+        try(RandomAccessFile f = new RandomAccessFile(fname, "rw")) {
+          Header h = Header.read(f);
+          BTree tree = new BTree(f, h);
+          PrintWriter pw = new PrintWriter(new FileWriter(csv));
+          tree.inorder((k, v) -> pw.println(Long.toUnsignedString(k) + "," + Long.toUnsignedString(v)));
+          System.out.println(csv + "... extracted to this location");
+        } catch (IOException e) {System.err.println("Erorr in extract: " + e.getMessage()); System.exit(1);}
         break;
 
       default:
